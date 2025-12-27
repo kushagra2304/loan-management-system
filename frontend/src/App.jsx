95ccc036a358a1fb5ca3c59c769418b2f9cd22a1
Kushagra <kushagrasharma826@gmail.com>
2025-12-27 23:03:26 +0530

    minor updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/NavBar"; 
 
 import LoanProducts from "./pages/LoanProducts";
 import LoanApplications from "./pages/LoanApplications";
